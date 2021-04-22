fc0c507e7b79622c15e77e73aa23c38d3e689b37
martindoleschel <83029160+martindoleschel@users.noreply.github.com>
2021-04-22 18:48:07 +0200

    Create saymynameintenthandler.js

---
+++ saymynameintenthandler.js
@@ -0,0 +1,14 @@
+const SayMyNameIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'SayMyNameIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hello John';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
